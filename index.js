950555df4f4dd08ec576ba88d0f585181efb6e4c
Pothula Hemanth <hemanthpothula8@gmail.com>
2023-09-27 23:08:05 +0530

    first commit

--- index.js
+++ index.js
@@ -1,76 +1,79 @@
 var express = require('express');
 var app = express();
-
-app.use(express.static("public"));
-
+const bodyParser = require("body-parser");
+const passwordhash = require("password-hash");
 const { initializeApp, cert } = require('firebase-admin/app');
-const { getFirestore} = require('firebase-admin/firestore');
+const { getFirestore } = require('firebase-admin/firestore');
 
-var serviceAccount = require("./key.json");
+var serviceAccount = require("./hem.json");
 
 initializeApp({
   credential: cert(serviceAccount)
 });
-0
+
 const db = getFirestore();
-  
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/signup', function (req, res) {  
-  res.sendFile(__dirname + "/public/" + "signup.html")  
+  res.sendFile(__dirname + "/public/signup.html");
 });
 
 app.get('/login', function (req, res) {  
-  res.sendFile(__dirname + "/public/" + "login.html") 
+  res.sendFile(__dirname + "/public/login.html");
 });
 
 app.get("/dashboard", function (req, res) {
-    res.sendFile(__dirname + "/public/" + "dashboard.html" );
+  res.sendFile(__dirname + "/public/dashboard.html");
 });
 
+app.post('/signupsubmit', function (req, res) {
+  const { username, email, password } = req.body;
 
-app.get('/signupsubmit', function (req, res) {
   db.collection("Details")
-    .where("email","==",req.query.email)
+    .where("email", "==", email)
     .get()
-    .then  ((docs)=>{
-      if(docs.size>0){
-        res.send("Hey user this account is already exist")
+    .then((docs) => {
+      if (docs.size > 0) {
+        res.send("Hey user, this account already exists");
+      } else {
+        db.collection("Details").add({
+          username: username,
+          email: email,
+          password: passwordhash.generate(password)
+        })
+        .then(() => {
+          const successMessage = "Signup Successful. Click here to <a href='/login'>Log in</a>.";
+          res.send(successMessage);
+        })
       }
-      else{
-  db.collection("Details").add({
-    username :req.query.username,
-    email: req.query.email,
-    password: req.query.password 
-  })
-  .then(() =>{
-    const successMessage="Signup Succesfull. click here to <a href='/login'>Log in</a>.";
-    res.send(successMessage);
-  });}
-});});
-
-app.get("/loginSubmit", function (req, res) {
-    const username = req.query.username;
-    const password = req.query.password;
-
-    db.collection("Details")
-    .where("username","==",username)
-    .where("password","==" ,password)
-    .get()
-    
-    .then  ((docs)=>{
-        if(docs.size>0){
-          res.redirect("/dashboard");
-        }
-        else{
-            res.send("login failed");
-        }
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-        res.send("An error occurred.");
-    });
+    })
 });
 
-  
-app.listen(3000, function () {  
-console.log('Example app listening on port 3000!')  
-})
+app.post("/loginSubmit", function (req, res) {
+  const { username, password } = req.body;
+
+  db.collection("Details")
+    .where("username", "==", username)
+    .get()
+    .then((docs) => {
+      var verified = false;
+      docs.forEach((doc) => {
+        verified = passwordhash.verify(password, doc.data().password);
+        if (verified) {
+          res.sendFile(__dirname + "/public/dashboard.html");
+        }
+      });
+
+      if (!verified) {
+        res.send("Login failed");
+      }
+    })
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
